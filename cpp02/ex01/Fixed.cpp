2e9054199388c71cf90f99de2954e594616d5177
luiz-dos <henrique8.lm@gmail.com>
2025-08-19 19:13:57 +0100

    comentario

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -39,7 +39,7 @@ Fixed::~Fixed()
 
 int Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
+	// std::cout << "getRawBits member function called" << std::endl;
 	return (this->value);
 }
 
