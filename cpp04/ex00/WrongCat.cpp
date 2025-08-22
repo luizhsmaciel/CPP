11dddfe80dba1052f5d1454e9a54fbdd1ca590b1
luiz-dos <henrique8.lm@gmail.com>
2025-08-22 17:55:57 +0100

    s

--- cpp04/ex00/WrongCat.cpp
+++ cpp04/ex00/WrongCat.cpp
@@ -4,29 +4,29 @@ WrongCat::WrongCat() : WrongAnimal()
 {
 	// char* teste = new char[100];
 	// (void)teste;
-	std::cout << "WrongCat Default Constructor called!\n";
+	std::cout << "❌ WrongCat Default Constructor called!\n";
 	this->type = "WrongCat";
-	std::cout << "\033[1;35m" <<"WrongCat " << this->type << " created!" << "\033[0m\n";
+	std::cout << "\033[1;35m" <<"❌ WrongCat " << this->type << " created!" << "\033[0m\n";
 }
 
 WrongCat::WrongCat(const WrongCat& other) : WrongAnimal(other)
 {
-	std::cout << "WrongCat Copy constructor called!\n";
-	std::cout << "\033[1;35m" <<"WrongCat " << this->type << " created!" << "\033[0m\n";
+	std::cout << "❌ WrongCat Copy constructor called!\n";
+	std::cout << "\033[1;35m" <<"❌ WrongCat " << this->type << " created!" << "\033[0m\n";
 }
 
 WrongCat& WrongCat::operator=(const WrongCat& other)
 {
-	std::cout << "WrongCat Copy assignment operator called\n";
+	std::cout << "❌ WrongCat Copy assignment operator called\n";
 	if (this != &other)
 		this->type = other.type;
-	std::cout << "\033[1;35m" <<"WrongCat " << this->type << " copied!" << "\033[0m\n";
+	std::cout << "\033[1;35m" <<"❌ WrongCat " << this->type << " copied!" << "\033[0m\n";
 	return (*this);
 }
 
 WrongCat::~WrongCat()
 {
-	std::cout << "WrongCat Destructor called\n";
+	std::cout << "❌ WrongCat Destructor called\n";
 }
 
 void	WrongCat::makeSound() const
