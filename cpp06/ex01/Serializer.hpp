580cf6ea28b376ee3cb789514ca0f2588887735d
luiz-dos <henrique8.lm@gmail.com>
2026-01-15 18:02:54 +0000

    ex01

---
+++ cpp06/ex01/Serializer.hpp
@@ -0,0 +1,20 @@
+#ifndef SERIALIZER_HPP
+#define SERIALIZER_HPP
+
+#include <stdint.h>
+
+class Serializer
+{
+	private:
+		Serializer();
+		Serializer(const Serializer& other);
+		Serializer& operator=(const Serializer& other);
+		~Serializer();
+
+	public:
+		// uintptr_t	serialize(Data* ptr);
+		// Data*		deserialize(uintptr_t raw);
+
+};
+
+#endif /* SERIALIZER_HPP */
---
+++ cpp06/ex01/main.cpp
@@ -0,0 +1,7 @@
+#include "Serializer.hpp"
+
+int	main(void)
+{
+
+	return (0);
+}
