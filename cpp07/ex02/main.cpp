146620ae3980cb09e25038f5e79c87ef1a334e78
luiz-dos <henrique8.lm@gmail.com>
2026-07-09 17:46:00 +0100

    cpp07 ex02

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -17,6 +17,10 @@ int	main(void)
 	std::cout << YELLOW << "│ constructor(n) │" << RESET << std::endl;
 	std::cout << YELLOW << "└────────────────┘" << RESET << std::endl;
 	Array<int> arrInt(5);
+	std::cout << CYAN_BOLD << "Default init: " << std::endl;
+	for (unsigned int i = 0; i < arrInt.size(); i++)
+		std::cout << "arrInt[" << i << "] = " << arrInt[i] << std::endl;
+	std::cout << RESET << std::endl;
 	for (unsigned int i = 0; i < arrInt.size(); i++)
 		arrInt[i] = i;
 	for (unsigned int i = 0; i < arrInt.size(); i++)
@@ -35,14 +39,24 @@ int	main(void)
 	std::cout << YELLOW << "└─────────────────────┘" << RESET << std::endl;
 	Array<int> arrAssign(3);
 	arrAssign = arrInt;
-	arrAssign[1] = 888;
 	std::cout << "arrInt[1] = " << arrInt[1] << " (deve ser 1)" << std::endl;
+	std::cout << "arrAssign[1] = " << arrAssign[1] << " (deve ser 1)" << std::endl;
+	arrAssign[1] = 888;
 	std::cout << "arrAssign[1] = " << arrAssign[1] << " (deve ser 888)" << std::endl;
 
 	std::cout << YELLOW << "┌───────────────┐" << RESET << std::endl;
 	std::cout << YELLOW << "│ Array<string> │" << RESET << std::endl;
 	std::cout << YELLOW << "└───────────────┘" << RESET << std::endl;
 	Array<std::string> arrStr(3);
+	std::cout << CYAN_BOLD << "Default init: " << std::endl;
+	for (unsigned int i = 0; i < arrStr.size(); i++)
+	{
+		if (arrStr[i].empty())
+			std::cout << "arrStr[" << i << "] = \"" << arrStr[i] << "\" (empty string)" << std::endl;
+		else
+			std::cout << "arrStr[" << i << "] = " << arrStr[i] << std::endl;
+	}
+	std::cout << RESET << std::endl;
 	arrStr[0] = "aviao";
 	arrStr[1] = "bola";
 	arrStr[2] = "casa";
