72a5bc20698c374ed238c1acd2a5f164ecbf8169
luiz-dos <henrique8.lm@gmail.com>
2026-07-27 11:21:06 +0100

    ex02

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -7,7 +7,10 @@ template <typename T>
 class MutantStack : public std::stack<T>
 {
 	public:
-		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::iterator				iterator;
+		typedef typename std::stack<T>::container_type::const_iterator			const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator		reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
 
 		MutantStack() : std::stack<T>() {}
 		MutantStack(const MutantStack& other) : std::stack<T>(other) {}
@@ -21,7 +24,15 @@ class MutantStack : public std::stack<T>
 
 		iterator begin() { return (this->c.begin()); }
 		iterator end() { return (this->c.end()); }
+		
+		const_iterator end() const { return (this->c.end()); }
+		const_iterator begin() const { return (this->c.begin()); }
 
+		reverse_iterator rbegin() { return (this->c.rbegin()); }
+		reverse_iterator rend() { return (this->c.rend()); }
+
+		const_reverse_iterator rbegin() const { return (this->c.rbegin()); }
+		const_reverse_iterator rend() const { return (this->c.rend()); }
 
 };
 
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,28 +1,35 @@
 #include "MutantStack.hpp"
 #include <iostream>
+#include <list>
+
+#define RESET "\033[0m"
+#define YELLOW "\033[1;33m"
 
 int	main(void)
 {
 	MutantStack<int> mstack;
-	
+
 	mstack.push(5);
 	mstack.push(17);
-	
+
 	std::cout << mstack.top() << std::endl;
-	
+
 	mstack.pop();
-	
+
 	std::cout << mstack.size() << std::endl;
-	
+
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
 	//[...]
 	mstack.push(0);
-	
+
+	std::cout << YELLOW << "┌──────────┐" << RESET << std::endl;
+	std::cout << YELLOW << "│ iterator │" << RESET << std::endl;
+	std::cout << YELLOW << "└──────────┘" << RESET << std::endl;
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
-	
+
 	++it;
 	--it;
 	while (it != ite)
@@ -30,4 +37,60 @@ int	main(void)
 		std::cout << *it << std::endl;
 		++it;
 	}
+	std::stack<int> s(mstack);
+	std::cout << "std::stack size: " << s.size() << std::endl;
+
+	std::cout << YELLOW << "┌──────────────────┐" << RESET << std::endl;
+	std::cout << YELLOW << "│ reverse_iterator │" << RESET << std::endl;
+	std::cout << YELLOW << "└──────────────────┘" << RESET << std::endl;
+	MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+	MutantStack<int>::reverse_iterator rite = mstack.rend();
+	for (; rit != rite; ++rit)
+		std::cout << *rit << std::endl;
+
+	std::cout << YELLOW << "┌────────────────┐" << RESET << std::endl;
+	std::cout << YELLOW << "│ const_iterator │" << RESET << std::endl;
+	std::cout << YELLOW << "└────────────────┘" << RESET << std::endl;
+	const MutantStack<int> mstackConst(mstack);
+	MutantStack<int>::const_iterator cit = mstackConst.begin();
+	MutantStack<int>::const_iterator cite = mstackConst.end();
+	for (; cit != cite; ++cit)
+		std::cout << *cit << std::endl;
+
+	std::cout << YELLOW << "┌────────────────────────┐" << RESET << std::endl;
+	std::cout << YELLOW << "│ const_reverse_iterator │" << RESET << std::endl;
+	std::cout << YELLOW << "└────────────────────────┘" << RESET << std::endl;
+	MutantStack<int>::const_reverse_iterator crit = mstackConst.rbegin();
+	MutantStack<int>::const_reverse_iterator crite = mstackConst.rend();
+	for (; crit != crite; ++crit)
+		std::cout << *crit << std::endl;
+
+	std::cout << YELLOW << "┌────────────────────────┐" << RESET << std::endl;
+	std::cout << YELLOW << "│ std::list<int> [tests] │" << RESET << std::endl;
+	std::cout << YELLOW << "└────────────────────────┘" << RESET << std::endl;
+	std::list<int> mlist;
+	mlist.push_back(5);
+	mlist.push_back(17);
+
+	std::cout << mlist.back() << std::endl;
+
+	mlist.pop_back();
+
+	std::cout << mlist.size() << std::endl;
+
+	mlist.push_back(3);
+	mlist.push_back(5);
+	mlist.push_back(737);
+	//[...]
+	mlist.push_back(0);
+
+	std::list<int>::iterator lit = mlist.begin();
+	std::list<int>::iterator lite = mlist.end();
+
+	++lit;
+	--lit;
+	for (; lit != lite; ++lit)
+		std::cout << *lit << std::endl;
+
+	return (0);
 }
